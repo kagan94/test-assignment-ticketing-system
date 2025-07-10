d399bfaf2bb515311618a9fd4158030786296cf9
leonid.dashko <leonidda@ext.inditex.com>
2025-07-10 14:15:26 +0300

    externalize redis properties to configuration

--- src/main/java/com/sportygroup/ticketingsystem/config/ApplicationConfiguration.java
+++ src/main/java/com/sportygroup/ticketingsystem/config/ApplicationConfiguration.java
@@ -1,8 +1,10 @@
 package com.sportygroup.ticketingsystem.config;
 
+import com.sportygroup.ticketingsystem.config.property.RedisProperties;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,13 +12,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackages = {"com.sportygroup.ticketingsystem.repository"})
+@ConfigurationPropertiesScan(basePackages = "com.sportygroup.ticketingsystem.config.property")
 @Configuration
 public class ApplicationConfiguration {
 
 	@Bean
-	public RedissonClient redissonClient() {
+	public RedissonClient redissonClient(RedisProperties redisProperties) {
 		final var config = new Config();
-		config.useSingleServer().setAddress("redis://localhost:6379");
+		config.useSingleServer().setAddress(redisProperties.getUrl());
 		return Redisson.create(config);
 	}
 }
---
+++ src/main/java/com/sportygroup/ticketingsystem/config/property/RedisProperties.java
@@ -0,0 +1,13 @@
+package com.sportygroup.ticketingsystem.config.property;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "redis")
+public class RedisProperties {
+
+	private String url;
+}
